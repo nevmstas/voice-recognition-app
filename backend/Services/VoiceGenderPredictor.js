ff43c884c262f2d42e8071391fae8b9406760f76
Stas <nevmyvakastas@gmail.com>
2020-12-17 23:35:55 +0600

    got result from model using tfjs

--- backend/Services/VoiceGenderPredictor.js
+++ backend/Services/VoiceGenderPredictor.js
@@ -1,32 +1,27 @@
 const tf = require("@tensorflow/tfjs");
+//require('@tensorflow/tfjs-node');
+
+const __devEndpoint = "http://localhost:5000";
 
 class VoiceGenderPredictor {
-    constructor(){
-        this.model;
-        this.modelPath = `file:///${__dirname}/../ResNet50/model.json`;
-    }
+  constructor() {
+    this.model = tf.loadLayersModel(`${__devEndpoint}/tfjsModel/model.json`);
+  }
 
-    initialize = async () => {
-        this.model = await tf.loadLayersModel(this.modelPath);
-    };
-
-    static create = async () =>{
-        const o = new VoiceGenderPredictor()
-        await o.initialize()
-        return o
-    }
-
-    getVoice = () =>{
-        return false
-    }
-    
-    classyfy = async () =>{
-        const voice = await this.getVoice()
-        const prediction = await this.model.predict(voice)
-
-        return prediction
-    }
+  classyfy = async (voiceArray) => {
+    this.model.then(
+      function (res) {
+        const shape = [1, 20];
+        const a = tf.tensor(voiceArray, shape);
+        const prediction = res.predict(a)
+        console.log(prediction)
+        console.log(prediction.toString())
+      },
+      function (err) {
+        return err;
+      }
+    );
+  };
 }
 
-module.exports = { VoiceGenderPredictor }
-
+module.exports = { VoiceGenderPredictor };
--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,34 @@ const PORT = config.get("port") || 5000
 // Create a new express app instance
 const app = express();
 
+app.use(express.static(__dirname + '/static'))
+
 app.get('/', function (req, res) {
-    const kek = new VoiceGenderPredictor()
-    const kek2 = kek.classyfy()
-    res.send(kek2);
+    const mock = [
+        0.0597809849598081,
+        0.0642412677031359,
+        0.032026913372582,
+        0.0150714886459209,
+        0.0901934398654331,
+        0.0751219512195122,
+        12.8634618371626,
+        274.402905502067,
+        0.893369416700807,
+        0.491917766397811,
+        0,
+        0.0597809849598081,
+        0.084279106440321,
+        0.0157016683022571,
+        0.275862068965517,
+        0.0078125,
+        0.0078125,
+        0.0078125,
+        0,
+        0,
+      ];
+    const predictor = new VoiceGenderPredictor()
+    predictor.classyfy(mock)
+    res.send('hello world');
 });
 
 async function start() {
